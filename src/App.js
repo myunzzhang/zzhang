b2750908a902a63cba539390ef9473102ab076a5
myunzzhang <kguswjd418@naver.com>
2023-06-07 17:25:40 +0900

    react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [username,setUsername]=useState("");
+  const [password,setPassword]=useState("");
+  const onSubmit = (e) => {
+    //form은 전송후에 refresh(새로고침)
+    e.preventDefault();
+    console.log(username, password)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+  <form onSubmit={onSubmit}>
+    <input placeholder="Usrname" value={username} onChange={(e)=>{setUsername(e.target.value)}}/><br/>
+    <input placeholder="Password" value={password} onChange={(e)=>{setPassword(e.target.value)}}/><br/>
+    <button type='submit'>Update</button>
+    </form>
+  </>
+  
   );
 }
 
